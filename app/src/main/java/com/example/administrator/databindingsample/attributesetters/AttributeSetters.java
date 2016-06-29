93afc33a6d80e5baab8a16d5ac53d324b4136e9e
lizhen <lizhen@zhumengyuan.com>
2016-06-29 10:31:09 +0800

    add Attribute Setters -Custom Setters

--- app/src/main/java/com/example/administrator/databindingsample/attributesetters/AttributeSetters.java
+++ app/src/main/java/com/example/administrator/databindingsample/attributesetters/AttributeSetters.java
@@ -3,11 +3,14 @@ package com.example.administrator.databindingsample.attributesetters;
 import android.app.Activity;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.administrator.databindingsample.R;
 import com.example.administrator.databindingsample.databinding.ActivityAttributeSettersBinding;
+import com.squareup.picasso.Picasso;
 
 
 public class AttributeSetters extends Activity {
@@ -17,14 +20,31 @@ public class AttributeSetters extends Activity {
         super.onCreate(savedInstanceState);
         ActivityAttributeSettersBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_attribute_setters);
         binding.setRadius(80);
+        binding.setImageUrl("http://img3.imgtn.bdimg.com/it/u=3461475865,399128756&fm=21&gp=0.jpg");
     }
 
+//    @BindingAdapter("android:paddingLeft")
+//    public static void setPaddingLeft(View view, int padding) {
+//        view.setPadding(padding,
+//                view.getPaddingTop(),
+//                view.getPaddingRight(),
+//                view.getPaddingBottom());
+//    }
+
     @BindingAdapter("android:paddingLeft")
-    public static void setPaddingLeft(View view, int padding) {
-        view.setPadding(padding,
-                view.getPaddingTop(),
-                view.getPaddingRight(),
-                view.getPaddingBottom());
+    public static void setPaddingLeft(View view, int oldPadding, int newPadding) {
+        if (oldPadding != newPadding) {
+            view.setPadding(newPadding,
+                    view.getPaddingTop(),
+                    view.getPaddingRight(),
+                    view.getPaddingBottom());
+        }
+    }
+
+    @BindingAdapter({"bind:imageUrl", "bind:error" })
+    public static void loadImage(ImageView view, String url, Drawable error) {
+        //Picasso.with(view.getContext()).load(url).error(error).into(view);
+        Picasso.with(view.getContext()).load(R.drawable.android).error(error).into(view);
     }
 
 }
